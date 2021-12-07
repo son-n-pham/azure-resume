136f1572876cc8474038ea1b712b48f03449cf84
son-n-pham <23143675@student.uwa.edu.au>
2021-12-08 00:21:41 +0800

    Save everything and push

---
+++ frontend/main.js
@@ -0,0 +1,19 @@
+window.addEventListener('DOMContentLoaded', (even) =>{
+    getVisitCount();
+})
+
+const functionApi = '';
+
+const getVisitCount = () => {
+    let count = 30;
+    fetch(functionApi).then(response => {
+        return Response.json()       
+    }).then(response => {
+        console.log("Website called function API.");
+        count = response.count;
+        document.getElementById("counter").innerText = count;
+    }).catch(function(error){
+        console.log(error);
+    });
+    return count;
+}
